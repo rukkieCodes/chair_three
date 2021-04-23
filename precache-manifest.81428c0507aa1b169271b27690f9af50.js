cbc1591d29b08f81318c26b6553ca8d0388e8a92
Terry <42579208+rukkieCodes@users.noreply.github.com>
2021-04-23 01:07:55 +0100

    Updates

---
+++ precache-manifest.81428c0507aa1b169271b27690f9af50.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "301c46cb93305a03645dd129991619cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11ce07bc146627214bb8",
+    "url": "/static/css/main.4a8ed910.chunk.css"
+  },
+  {
+    "revision": "fadad7c09980acab909f",
+    "url": "/static/js/2.4a5f5718.chunk.js"
+  },
+  {
+    "revision": "c9b31f27ce0803bcbda04bfd0eba249e",
+    "url": "/static/js/2.4a5f5718.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11ce07bc146627214bb8",
+    "url": "/static/js/main.639a16b1.chunk.js"
+  },
+  {
+    "revision": "142b697ed77b435eaccc",
+    "url": "/static/js/runtime-main.efa8e349.js"
+  }
+]);
